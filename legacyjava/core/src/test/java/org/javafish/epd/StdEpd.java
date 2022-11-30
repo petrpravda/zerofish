a139a0d6836fdb88700877e6dd629742de258734
Petr Pravda <petr.pravda@gmail.com>
2022-11-30 21:51:28 +0100

    epd tests

--- legacyjava/core/src/test/java/org/javafish/epd/StdEpd.java
+++ legacyjava/core/src/test/java/org/javafish/epd/StdEpd.java
@@ -4,9 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StdEpd {
-    public record EpdLine(String fen, String bestMove) {};
+    public record StsDesc(String section, Integer number) {}
 
-    private static final Pattern REGEX = Pattern.compile("(?<fenStart>\\w+(\\/\\w+){7} [wb] [KQkq-]+ [a-h1-8-]+) bm (?<bestMove>[\\w-+]+)");
+    public record EpdLine(String fen, String bestMove, StsDesc desc) {}
+
+    private static final Pattern REGEX_STS_DESC = Pattern.compile("id \".+? (?<section>.+)\\.(?<number>\\d+)\"");
+    private static final Pattern REGEX = Pattern.compile("(?<fenStart>\\w+(\\/\\w+){7} [wb] [KQkq-]+ [a-h1-8-]+) bm (?<bestMove>[\\w-+]+)(?<rest>.+)");
     public static EpdLine parseLine(String line) {
         Matcher matcher = REGEX.matcher(line);
         if (!matcher.find()) {
@@ -14,9 +17,20 @@ public class StdEpd {
         } else {
             String fenStart = matcher.group("fenStart");
             String bestMove = matcher.group("bestMove");
-            EpdLine epd = new EpdLine(fenStart, bestMove);
-            return epd;
+            String rest = matcher.group("rest");
+            StsDesc stsDesc = parseDescriptor(rest);
+            return new EpdLine(fenStart, bestMove, stsDesc);
         }
+    }
+
+    private static StsDesc parseDescriptor(String rest) {
+        Matcher matcher = REGEX_STS_DESC.matcher(rest);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException();
+        }
+        String section = matcher.group("section");
+        Integer number = Integer.valueOf(matcher.group("number"));
+        return new StsDesc(section, number);
     }
 
     public static void main(String[] args) {
