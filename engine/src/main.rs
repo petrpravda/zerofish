c24fb282cd7a87aab05f2177627460bfb26d55ec
Petr Pravda <petr.pravda@gmail.com>
2023-02-10 22:19:21 +0100

    main.rs file cleanup

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -8,23 +8,6 @@ use zerofish::{engine_thread, uci};
 use zerofish::engine::{EngineOptions};
 use zerofish::util::extract_parameter;
 
-//use zerofish::piece_square_table::MGS;
-
-// mod uci;
-// mod engine;
-// mod engine_thread;
-// mod fen;
-// mod bitboard;
-// mod piece;
-// mod side;
-// mod square;
-// mod r#move;
-// mod lib;
-// mod board_state;
-// mod board_state;
-//use zerofish::{engine_thread, uci};
-
-
 fn main() {
     let args_strings = args().collect::<Vec<String>>();
     let args: Vec<&str> = args_strings.iter().map(|s| s.as_ref()).collect();
@@ -33,20 +16,4 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "full");
     let stop_signal = Arc::new(AtomicBool::new(false));
     uci::start_uci_loop(&engine_thread::spawn_engine_thread(&engine_options, stop_signal.clone()).0, stop_signal.clone());
-
-
-
-    // let mut engine = Engine::new(EngineOptions { log_filename:  None});
-    // engine.do_pgn_moves("d4 Nf6 c4 e6 Nc3 Bb4 e3 O-O Bd3 c5");
-    // println!("{}", engine.position.to_fen());
-
-    // let mut search = Search::new();     //println!("{}", MGS[1][3]);
-    // let position = BoardPosition::from_fen(START_POS);
-    // let result = search.it_deep(&position, 10);
-    // println!("{:?}", result);
-    // uci::start_uci_loop(&engine_thread::spawn_engine_thread());
-    // let bitboard = Bitboard::new();
-    // let mut state = from_fen_default(START_POS, &bitboard);
-    // let moves = state.generate_legal_moves();
-    // println!("{}", moves);
 }
