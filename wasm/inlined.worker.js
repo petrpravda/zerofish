7a22260ce39ab40e7c2069f678b0e46db69ea971
Petr Pravda <petr.pravda@gmail.com>
2023-02-10 22:21:56 +0100

    inlined.worker.js

--- wasm/inlined.worker.js
+++ wasm/inlined.worker.js
@@ -2,10 +2,21 @@ const { newEngineContext, executeUciCommand } = wasm_bindgen;
 
 async function run() {
     await wasm_bindgen('./zerofish.wasm');
-    const engineContext = newEngineContext(self);
+    const buffer = new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT);
+    const sharedArray = new Int32Array(buffer);
+    Atomics.store(sharedArray, 0, 0);
+
+    const engineContext = newEngineContext(self, sharedArray);
     onmessage = function (e) {
-        executeUciCommand(e.data, engineContext);
+        // console.info(`processing ${e.data}`);
+        const cmd = e.data; //.startsWith('stop') ? 'stop' : e.data;
+        if (cmd !== 'stop') {
+            Atomics.store(sharedArray, 0, 0);
+        }
+        executeUciCommand(cmd, engineContext);
+        // console.info(`finished processing ${cmd}`);
     }
+    postMessage(buffer);
 }
 
 run().then(() => {});
