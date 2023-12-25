6a7569bac8bc7e232b2a1c747cf65bdaea26d01b
Petr Pravda <petr.pravda@gmail.com>
2023-12-25 21:33:09 +0100

    tensorflow installed

---
+++ gameplayagent/src/hf/environmentVariable.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const HF_ACCESS_TOKEN = process.env.HF_ACCESS_TOKEN;
---
+++ gameplayagent/src/tf/tfHelloWorld.ts
@@ -0,0 +1,11 @@
+import * as tf from '@tensorflow/tfjs';
+
+// Define two 2x2 matrices.
+let matrix1 = tf.tensor2d([1, 2, 3, 4], [2, 2]);
+let matrix2 = tf.tensor2d([5, 6, 7, 8], [2, 2]);
+
+// Multiply the matrices.
+let result = tf.matMul(matrix1, matrix2);
+
+// Print the result.
+result.print();
