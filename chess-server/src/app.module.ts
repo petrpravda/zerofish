05b5aa1d91cb4132b6d23ca93ab5928df0507656
Petr Pravda <petr.pravda@gmail.com>
2024-01-21 09:21:41 +0100

    static-serve, favicon.ico

--- chess-server/src/app.module.ts
+++ chess-server/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+const rootPath = join(__dirname, '..', 'static');
+console.info(rootPath);
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: rootPath,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
