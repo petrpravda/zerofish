07547a143854471a3c6aefe71ef8f6bc2b169d37
Petr Pravda <petr.pravda@gmail.com>
2024-06-29 18:29:38 +0200

    maven project modularization

--- futurejava/nnue-training/src/main/java/nnue/Constants.java
+++ futurejava/nnue-training/src/main/java/nnue/Constants.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Constants {
     public static final short[] INPUT_LAYER_WEIGHT = loadWeights("nnue/input.layer.weights.txt");
